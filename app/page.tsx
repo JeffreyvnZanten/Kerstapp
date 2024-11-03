6423d45af5a77aff380e54f2963a863bdf7ac898
jeffreyvnzanten <j.van.zanten@gmail.com>
2024-11-03 18:51:40 +0100

    Image test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <>HelloWorld</>
+  return (
+    <div className="bg-[url('/xmas1.webp')] bg-cover bg-center bg-no-repeat min-h-screen w-full fixed inset-0">
+      HelloWorld
+    </div>
+  )
 }
